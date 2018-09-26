bd90daba0018ba737278d6778366693b1db470dd
Hubert Kozłowski <hubert890@poczta.onet.pl>
2018-09-26 19:23:06 +0200

    functionalities

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,19 +1,21 @@
 'use strict';
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass({
-		render: function() {
-				return (
-					<div className='jumbotron'>
-						<h1>Welcome to the React Flux Starter Kit</h1>
-						<p>
-							React, Flux, and React Router combined with Browserify, Gulp, and Bootstrap. Build a React and Flux app from scratch using this starter kit on <a href="http://www.pluralsight.com/courses/react-flux-building-applications">Pluralsight</a>
-						</p>
-					</div>
-				);
-		}
+	render: function() {
+		return (
+			<div className='jumbotron'>
+				<h1>Pluralsight Administration</h1>
+				<p>
+					React, React Router and Flux for ultra-responsive web apps
+				</p>
+				<Link to='about' className='btn btn-primary btn-lg'>Learn more</Link>
+			</div>
+		);
+	}
 });
-
 
 module.exports = Home;
